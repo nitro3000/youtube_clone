d4d9c3f31d8e57398d5e5fedf187ee38a92c7e0f
andrey <andreyfaigon@gmail.com>
2023-02-24 15:48:23 -0500

    added first components

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+//hooks
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import {SearchIcon} from  '@mui/icons-material';
+
+
+const SearchBar = () => {
+  return (
+    <Paper 
+    component="form" on>
+        SearchBar
+    </Paper>
+  )
+}
+
+export default SearchBar
