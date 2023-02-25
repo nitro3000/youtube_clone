e7abc5590a1e1f6d537d7eb28cbb295d2474b9cb
andrey <andreyfaigon@gmail.com>
2023-02-25 10:47:49 -0500

    first buttons

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,16 @@ import {SearchIcon} from  '@mui/icons-material';
 const SearchBar = () => {
   return (
     <Paper 
-    component="form" on>
+        component="form" 
+        onSubmit={()=>{}}
+        sx={{
+            borderRadius:20,
+            border:'1px solis #e3e3e3',
+            pl:2,
+            boxShadow:'none',
+            marginRight:{sm: 5}
+        }}
+    > 
         SearchBar
     </Paper>
   )
